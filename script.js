79ca0244dbb68eff7089f3f36e6240da7b6663c5
Bryan Maciel <bryanmacieldealmeida06@gmail.com>
2025-04-26 17:19:32 -0300

    feat: melhorias no layout, responsividade e design
    - Ajustes no navbar para fixação centralizada e responsividade.
    - Atualização do estilo dos botões com gradientes e transições mais suaves.
    - Alterações no footer com ajustes de cor e responsividade.
    - Implementação de nova paleta de cores para o site, mantendo o estilo dark.
    - Correção de sombras e refinamento visual dos elementos.
    - Ajustes na exibição de imagens (bandeiras nos telefones, etc.).
    - Revisão de responsividade para telas menores (ajustes no footer, navbar, etc.)
    - próximas atualizações: implementação de alterar idioma da página, currículo para baixar, melhoria no design e melhor organização do código.

--- script.js
+++ script.js
@@ -7,30 +7,22 @@ const objetivo = document.querySelector('.objetivo');
 const idade = document.querySelector('.idade');
 const localizacao = document.querySelector('.local');
 const foto = document.querySelector('.foto-apresentacao');
+const projetos = document.querySelector('.projeto');
+const exp = document.querySelector('.experience');
 
-function english() {
-}
-function portugues() {
-
-}
-
-function espanol() {
-
-}
 
 fetch('pt.json')
     .then(response => response.json())
     .then(data => {
-
+        objetivo.innerHTML = data.goals;
+        foto.src = data.imagem;
         user.innerHTML = data.nome;
         idade.innerHTML = data.age;
         curso.innerHTML = data.curso;
         faculdade.innerHTML = data.faculdade;
         localizacao.innerHTML = data.location;
         cargo.innerHTML = data.cargo;
-        objetivo.innerHTML = data.goals;
         skills.innerHTML = data.skills.join("<br>");
-        foto.src = data.imagem;
-
-
+        projetos.innerHTML = data.projects.join("<br>");
+        exp.innerHTML = data.experiences.join("<br>");
     });
